c40c479bf80327b7f0223244b4f8611088a6697f
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2017-08-21 15:48:54 -0600

    🤞🏻

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const routes = require('./routes/paleoPal_dinosaur.js')
 
 const app = express()
 const port = process.env.PORT || 3000
